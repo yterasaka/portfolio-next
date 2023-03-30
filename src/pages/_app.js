8797210f55f3f2b0faf6042995d215828848f129
yterasaka <yuki.terasaka@gmail.com>
2023-03-30 15:03:35 +0200

    Component Files Preparation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Yuki Terasaka: Frontend Developer</title>
+        <meta name="description" content="Yuki's Web Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
